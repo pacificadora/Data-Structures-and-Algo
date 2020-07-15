9850ab9426cf88fcb72adcee3e77a415994ecb8f
pacificadora <44291397+pacificadora@users.noreply.github.com>
2020-07-15 11:35:24 +0530

    Create fibonacci.py

---
+++ Classic DP problems/fibonacci.py
@@ -0,0 +1,15 @@
+#fibonacci using memoization
+def fibonacci(n):
+  a=0
+  b=1
+  if n<=1:
+    return n
+  else:
+    c=a+b
+    a=b
+    b=c
+  return b
+  
+  n=int(input())
+  res = fibonacci(n)
+  print(n)
